61d0b1b6315285726ce5318e3965a9ccc11c0672
Johny Babylon <Home@Johnys-MacBook.local>
2018-12-18 14:02:15 -0600

    added calcbtn & text fields

---
+++ WindowShopper/View/CurrencyTextField.swift
@@ -0,0 +1,44 @@
+//
+//  CurrencyTextField.swift
+//  WindowShopper
+//
+//  Created by Johny Babylon on 12/18/18.
+//  Copyright © 2018 Johny Babylon. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrencyTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        let place = NSAttributedString(string: placeholder!, attributes:[.foregroundColor: UIColor.white])
+        attributedPlaceholder = place
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+
+}
